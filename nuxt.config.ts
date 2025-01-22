9c596d0c19b07c7f8f6d4556aa55286f6fc7cbf8
sebastiandotdev <sebastiandotdev@gmail.com>
2025-01-21 21:29:22 -0500

    Add application title and icons to Nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Downloader',
+      link: [
+        { rel: 'icon', type: 'svg/x-icon', href: '/logo.svg' },
+      ],
+    },
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@unocss/nuxt'],
