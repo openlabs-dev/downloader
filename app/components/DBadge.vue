e5d29521b9020f1e5c05098c2c0268bd7aa82756
sebastiandotdev <sebastiandotdev@gmail.com>
2025-01-22 21:34:14 -0500

    Add DBadge component with class variance authority

---
+++ app/components/DBadge.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { cva, type VariantProps } from 'class-variance-authority'
+
+interface BadgeVariantProps extends VariantProps<typeof badgeVariants> {}
+
+interface BadgeProps {
+  variant?: BadgeVariantProps['variant']
+}
+
+defineProps<BadgeProps>()
+
+const badgeVariants = cva('select-none whitespace-nowrap rounded', {
+  variants: {
+    variant: {
+      default: 'bg-primary text-white',
+      secondary: 'bg-secondary text-white',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+})
+</script>
+
+<template>
+  <span text="center xs" p="x-1.5 y-0.5" mx="0.5" :class="badgeVariants({ variant })">
+    <slot />
+  </span>
+</template>
--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig, presetAttributify, presetUno, transformerDirectives } from 'unocss'
 
 export default defineConfig({
+  theme: {
+    colors: {
+      primary: '#BF3434',
+      secondary: '#152B46',
+    },
+  },
   presets: [presetAttributify(), presetUno()],
   transformers: [transformerDirectives()],
 })
