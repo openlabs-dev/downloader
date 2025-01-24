f521899c91ac90c6bcf9b4f20abbab83319650df
Gerzon05 <gerzonarletrangel@gmail.com>
2025-01-23 22:04:33 -0500

    Added header in process

---
+++ app/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header class='flex justify-between items-center p-4 sticky top-0 left-0 right-0 border-b border-black bg-white z-20'>
+    <img src="/logo.svg" alt="Logo" class='size-8'
+      hover='rotate-[180deg] duration-[.5s] ease-in-out animate-iteration-count-1' />
+    <DButton variant='ghost'>Sign In</DButton>
+  </header>
+</template>
