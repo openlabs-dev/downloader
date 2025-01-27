592d99793663899c041ff9162e94536ad3d23db6
sebastiandotdev <sebastiandotdev@gmail.com>
2025-01-27 18:18:31 -0500

    Replace logo image with Hero component

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,9 +3,8 @@
     <main isolate flex flex-1 flex-col items-center gap-2 overflow-auto p-4>
       <div class="flex-[1_1_0%]" />
       <div max-w-5xl w-full class="flex-[1_0_85%]" flex flex-col items-center gap-4>
-        <figure mb-4 class="max-w-[90%]">
-          <NuxtImg src="/logo-x2.svg" width="600" />
-        </figure>
+        <Hero />
+        <Search />
         <Footer />
       </div>
     </main>
