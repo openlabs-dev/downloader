102357e93c372c911d07256a70307b341049dada
sebastiandotdev <sebastiandotdev@gmail.com>
2025-01-27 18:18:22 -0500

    Add Search component with basic form structure

---
+++ app/components/Search.vue
@@ -0,0 +1,14 @@
+<template>
+  <div w-full>
+    <form action="/search" method="get" relative isolate w-full>
+      <input type="text" hidden>
+      <div
+        class="dw-border-base z-1 min-h-[32px] flex flex-1 items-center gap-[.25rem] rounded"
+        px=".25rem"
+        relative
+      >
+        <span>Search folder</span>
+      </div>
+    </form>
+  </div>
+</template>
