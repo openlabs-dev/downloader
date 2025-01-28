8082c141f3422f26405bb237fd3ea09efca9de92
sebastiandotdev <sebastiandotdev@gmail.com>
2025-01-27 21:50:16 -0500

    Improve navigation structure

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,10 @@
 <template>
-  <NuxtLayout>
-    <main isolate flex flex-1 flex-col items-center gap-2 overflow-auto p-4>
-      <div class="flex-[1_1_0%]" />
-      <div max-w-5xl w-full class="flex-[1_0_85%]" flex flex-col items-center gap-4>
-        <Hero />
-        <Search />
-        <Footer />
-      </div>
-    </main>
-  </NuxtLayout>
+  <WrapperNav>
+    <NuxtLink to="/">
+      <NuxtImg src="/logo-x2.svg" alt="Logo" width="200" />
+    </NuxtLink>
+    <DButton to="/downloader" variant="primary">
+      Downloader
+    </DButton>
+  </WrapperNav>
 </template>
