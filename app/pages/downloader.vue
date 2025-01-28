0eeac8371b97f823ca17199bad8e51a8b50cc6c9
sebastiandotdev <sebastiandotdev@gmail.com>
2025-01-27 21:49:46 -0500

    Add downloader page with layout and components

---
+++ app/pages/downloader.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLayout>
+    <div isolate flex flex-1 flex-col items-center gap-2 overflow-auto>
+      <div class="flex-[1_1_0%]" />
+      <div max-w-5xl w-full class="flex-[1_0_85%]" flex flex-col items-center gap-4>
+        <Hero />
+        <Search />
+        <Footer />
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
