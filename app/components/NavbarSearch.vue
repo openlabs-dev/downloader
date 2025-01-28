14e4765510b2235af3b83a5cad966eab603832b8
sebastiandotdev <sebastiandotdev@gmail.com>
2025-01-27 21:48:43 -0500

    Into NavbarSearch for improved structure

---
+++ app/components/NavbarSearch.vue
@@ -0,0 +1,12 @@
+<template>
+  <WrapperNav>
+    <NuxtImg
+      src="/logo.svg" alt="Logo"
+      width="32"
+      hover="rotate-[180deg] duration-[.5s] ease-in-out animate-iteration-count-1"
+    />
+    <DButton variant="ghost">
+      Sign In
+    </DButton>
+  </WrapperNav>
+</template>
