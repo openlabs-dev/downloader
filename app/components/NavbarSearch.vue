9aa95a4b85ab11d1b1de9e949c2660a7ecb22ddd
Gerzon05 <gerzonarletrangel@gmail.com>
2025-01-28 21:28:33 -0500

    The NavbarSearch component is in beta

--- app/components/NavbarSearch.vue
+++ app/components/NavbarSearch.vue
@@ -1,12 +1,23 @@
 <template>
   <WrapperNav>
-    <NuxtImg
-      src="/logo.svg" alt="Logo"
-      width="32"
-      hover="rotate-[180deg] duration-[.5s] ease-in-out animate-iteration-count-1"
-    />
-    <DButton variant="ghost">
-      Sign In
-    </DButton>
+    <div flex gap-6>
+      <figure m-0>
+        <NuxtImg
+          src="/logo.svg" alt="Logo" width="32"
+          hover="rotate-[180deg] duration-[.5s] ease-in-out animate-iteration-count-1"
+        />
+      </figure>
+      <nav>
+        <DButton variant="ghost" gap-2>
+          <div class="i-ic-baseline-search" /> Search
+        </DButton>
+      </nav>
+    </div>
+    <div flex items-center gap-2>
+      <DBadge>Beta</DBadge>
+      <DButton variant="ghost">
+        Sing In
+      </DButton>
+    </div>
   </WrapperNav>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,7 @@
     <NuxtLink to="/">
       <NuxtImg src="/logo-x2.svg" alt="Logo" width="200" />
     </NuxtLink>
-    <DButton to="/downloader" variant="primary">
+    <DButton to="/downloader" variant="default">
       Downloader
     </DButton>
   </WrapperNav>
