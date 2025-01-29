f2df673fc01f39f165061fd1c14e71c531df4679
sebastiandotdev <sebastiandotdev@gmail.com>
2025-01-28 22:24:24 -0500

    Enhance Search with improved

--- app/components/Search.vue
+++ app/components/Search.vue
@@ -1,13 +1,24 @@
 <template>
   <div w-full>
     <form action="/search" method="get" relative isolate w-full>
-      <input type="text" hidden>
       <div
-        class="dw-border-base z-1 min-h-[32px] flex flex-1 items-center gap-[.25rem] rounded"
-        px=".25rem"
+        class="z-1 min-h-[35px] flex flex-1 items-center gap-3 rounded"
+        px="0.75rem"
         relative
       >
-        <span>Search folder</span>
+        <input
+          type="text"
+          placeholder="Search..."
+          name="q"
+          p="3"
+          bg="transparent"
+          w-full
+          rounded="lg"
+          class="border-1 border-neutral/40 border-solid outline-none transition-all duration-200 focus:border-transparent focus:ring-2 focus:ring-primary"
+        >
+        <DButton type="submit" variant="secondary">
+          <div class="i-ic-outline-search" text-xl />
+        </DButton>
       </div>
     </form>
   </div>
