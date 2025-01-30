4968575f4516d2b6d7e8d9956e7edf1465971aa2
sebastiandotdev <sebastiandotdev@gmail.com>
2025-01-29 22:28:29 -0500

    Replace logo with `<Logo />`

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,11 +2,31 @@
   <NuxtLayout>
     <WrapperNav>
       <NuxtLink to="/">
-        <NuxtImg src="/logo-x2.svg" alt="Logo" width="200" />
+        <Logo />
       </NuxtLink>
       <DButton to="/downloader" variant="default">
         Downloader
       </DButton>
     </WrapperNav>
+    <header>
+      <div class="mx-auto px-6 py-16 text-center container">
+        <h1 class="mx-auto mb-6 max-w-5xl text-balance text-6xl font-bold">
+          Tu descargador de confianza sin interrupciones
+        </h1>
+        <p class="mx-auto mb-8 max-w-2xl text-xl text-gray-600">
+          Accede y descarga cualquier tipo de contenido de manera rápida y segura.
+          Optimizado para desarrolladores y creadores de contenido.
+        </p>
+        <div class="flex justify-center space-x-4">
+          <DButton to="#">
+            Conocer más
+          </DButton>
+          <DButton to="#" variant="ghost">
+            Ver Documentación
+          </DButton>
+        </div>
+      </div>
+    </header>
+    <Feature />
   </NuxtLayout>
 </template>
