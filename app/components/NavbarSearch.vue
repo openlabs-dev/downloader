7d076185401c92545131a2b3532435d4f1cf029d
Gerzon05 <gerzonarletrangel@gmail.com>
2025-02-02 21:17:40 -0500

    Added NavbarSearch enhancements

--- app/components/NavbarSearch.vue
+++ app/components/NavbarSearch.vue
@@ -2,22 +2,44 @@
   <WrapperNav>
     <div flex gap-6>
       <figure m-0>
-        <NuxtImg
-          src="/logo.svg" alt="Logo" width="32"
-          hover="rotate-[180deg] duration-[.5s] ease-in-out animate-iteration-count-1"
-        />
+        <NuxtImg src="/logo.svg" alt="Logo" width="32" id='animate-rotate' />
       </figure>
       <nav>
-        <DButton variant="ghost" gap-2>
-          <div class="i-ic-baseline-search" /> Search
+        <DButton to="#" class="flex-row-reverse shadow-none" variant="ghost">
+          Documentation
+        </DButton>
+        <DButton to="#" class="flex-row-reverse shadow-none" variant="ghost">
+          About downloader
         </DButton>
       </nav>
     </div>
     <div flex items-center gap-2>
       <DBadge>Beta</DBadge>
-      <DButton variant="ghost">
-        Sing In
+      <DButton to="#">
+        Login
+      </DButton>
+      <DButton to="#" variant="ghost">
+        Sign Up
       </DButton>
     </div>
   </WrapperNav>
 </template>
+
+<style scooped>
+  @keyframes rotate {
+    0% {
+      transform: scale(1);
+      transform: rotate(0);
+    }
+
+    100% {
+      transform: scale(1.2);
+      transform: rotate(180deg);
+    }
+  }
+
+  #animate-rotate:hover {
+    animation: rotate .5s ease-out;
+  }
+
+</style>
--- app/components/WrapperNav.vue
+++ app/components/WrapperNav.vue
@@ -1,7 +1,5 @@
 <template>
-  <header
-    flex="~ items-center justify-between" sticky="left-0 right-0 top-0 z-10" p-2.5
-  >
+  <header flex="~ items-center justify-between" xl='container mx-auto w-full' sticky="left-0 right-0 top-0 z-10" p-2.5>
     <slot />
   </header>
 </template>
